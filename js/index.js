4773e1b641de19449e45663d5fd47648d9483c1c
wenzi <baibai194@gmail.com>
2015-12-01 23:34:05 +0800

    调整时间轴和皇帝轴

--- js/index.js
+++ js/index.js
@@ -18,20 +18,20 @@
 		}
 	}
 
-	var year_start = 1200, // 开始年份
-		year_end = 1600, // 结束年份
-		year_per = 2; // 每年占用的像素值
+	var year_start = 1350, // 开始年份
+		year_end = 1680,   // 结束年份
+		year_per = 20,      // 每年占用的像素值
+		year_degree = 2;  // 跳跃的刻度
 
 	function setYearLine(){
-		var html = '<div class="year"><ul>',
-			per = 10;
-		for(var i=year_start; i<year_end; i=i+per){
+		var html = '<div class="year"><ul>';
+		for(var i=year_start; i<year_end; i=i+year_degree){
 			html += '<li>'+i+' -</li>';
 		}
 		html += '</ul></div>';
 
 
-		$('#content').append(html).find('li').css({'margin-bottom':year_per*per});
+		$('#content').append(html).find('li').css({'height':year_per*year_degree+'px', 'line-height':year_per*year_degree+'px'});
 	}
 	setYearLine();
 	setEmperorLine();
@@ -39,24 +39,31 @@
 	function setEmperorLine(){
 		var data = [
 			{
-				start : 1230,
-				end : 1280,
-				name : '朱元璋'
+				start : 1368,
+				end : 1398,
+				miao : '明太祖',
+				name : '朱元璋',
+				nian : '洪武',
+				info : '开国皇帝；惩治贪官，怒杀功臣',
 			},
 			{
-				start : 1280,
-				end : 1320,
-				name : '朱允炆'
+				start : 1398,
+				end : 1420,
+				miao : '明惠宗',
+				name : '朱允炆',
+				nian : '建文',
+				info : '朱元璋嫡长孙，温文孝顺'
 			}
 		];
 
-		var html = '<div class="line"><ul>';
+		var marginTop = (data[0].start-year_start)*year_per+year_per*year_degree/2;
+		var html = '<div class="line"><ul style="margin-top:'+marginTop+'px">';
 		for(var i=0; i<data.length; i++){
 			var item = data[i],
 				height = (item.end-item.start)*year_per,
 				bgcolor = util.randomColor();
-			html += '<li style="height:'+height+'px; background:'+bgcolor+';" title="'+ item.name +'"></li>';
+			html += '<li style="height:'+height+'px; background:'+bgcolor+';" title="'+ item.name +'('+item.start+'-'+item.end+')"><div><h2 class="name">'+item.miao+':'+item.name+'（'+item.nian+'）</h2><p class="info">'+item.info+'</p></div></li>';
 		}
-		$('#content').append(html);
+		$('#content').append(html).children('.line').css({'height':(year_end-year_start)*year_per});
 	}
 })();
