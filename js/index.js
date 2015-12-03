4b7cc94ed737d216902aa71d4a22c6b6aa9192db
wenzi <baibai194@gmail.com>
2015-12-03 23:44:12 +0800

    添加事件轴

--- js/index.js
+++ js/index.js
@@ -15,6 +15,11 @@
 				strHex += arrHex[index];
 			}
 			return strHex;
+		},
+
+		// 获取当前年份所对应的像素
+		getItemPix : function(now){
+			return (now-year_start)*year_per+year_per*year_degree/2;
 		}
 	}
 
@@ -31,7 +36,7 @@
 		html += '</ul></div>';
 
 
-		$('#content').append(html).find('li').css({'height':year_per*year_degree+'px', 'line-height':year_per*year_degree+'px'});
+		$('#content').append(html).children('.year').find('li').css({'height':year_per*year_degree+'px', 'line-height':year_per*year_degree+'px'});
 	}
 	setYearLine();
 	setEmperorLine();
@@ -56,14 +61,60 @@
 			}
 		];
 
-		var marginTop = (data[0].start-year_start)*year_per+year_per*year_degree/2;
+		var marginTop =  util.getItemPix(data[0].start);
 		var html = '<div class="line"><ul style="margin-top:'+marginTop+'px">';
 		for(var i=0; i<data.length; i++){
 			var item = data[i],
 				height = (item.end-item.start)*year_per,
 				bgcolor = util.randomColor();
-			html += '<li style="height:'+height+'px; background:'+bgcolor+';" title="'+ item.name +'('+item.start+'-'+item.end+')"><div><h2 class="name">'+item.miao+':'+item.name+'（'+item.nian+'）</h2><p class="info">'+item.info+'</p></div></li>';
+			html += '<li style="height:'+height+'px; background:'+bgcolor+';">\
+						<div>\
+							<h2 class="name">'+item.miao+':'+item.name+'（'+item.nian+'）</h2>\
+							<p class="info">'+item.info+'</p>\
+							<p>在位：'+item.start+'-'+item.end+'('+(item.end-item.start)+'年)</p>\
+						</div>\
+					</li>';
 		}
+		html += '</ul></div>';
+		
 		$('#content').append(html).children('.line').css({'height':(year_end-year_start)*year_per});
 	}
+
+	function setThingLine(){
+		var data = [
+			{
+				start : 1368,
+				end : 1450,  // end可有可无，视情况而定，若不是该事件没有时间跨度，则可以省略
+				title : '朱元璋在南京登基，建国大名，年号洪武',
+				info : '',  // info可有可无，若事件过于简单，可省略
+			},
+			{
+				start : 1369,  // 公历
+				nian_start : '洪武二年', // 皇帝年号计数
+				end : 1400,
+				nian_end : '建文二年',
+				title : '大将常遇春在北伐时暴病而亡，享年39岁'
+			}
+		];
+
+		var html = '<div class="thing">';
+		for(var i=0; i<data.length; i++){
+			var item = data[i],
+				startTop = util.getItemPix(item.start),
+				endTop = util.getItemPix(item.end),
+				bgcolor = util.randomColor();
+			html += '<div class="item" style="top:'+startTop+'px; height:'+(endTop-startTop)+'px; border-color:'+bgcolor+'; color:'+bgcolor+'">\
+						<span class="start">'+item.start+'('+item.nian_start+')</span>\
+						<div class="proce" style="background:'+bgcolor+'"></div>\
+						<div class="info">\
+							<h3>'+item.title+'</h3>\
+							<p class="desp">'+ (item.info||'' ) +'</p>\
+						</div>\
+						<span class="end">'+item.end+'('+item.nian_end+')</span>\
+					</div>';
+		}
+		html += '</div>';
+		$('#content').append(html)
+	}
+	setThingLine();
 })();
